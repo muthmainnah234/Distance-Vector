f4fc4ae7dead386c879acef7f4c5e6267a24e704
franciscoken <franciscokenandi@gmail.com>
2017-11-30 13:13:03 +0700

    jalan sesuai testcase spek

--- dvector.cpp
+++ dvector.cpp
@@ -98,7 +98,7 @@ int main() {
 
 	cin >> N >> M;
 
-	cout << "CHECKPOINT 1: READ SKENARIO DAN TOPOLOGI"<<endl;
+	//cout << "CHECKPOINT 1: READ SKENARIO DAN TOPOLOGI"<<endl;
 
 	top = new Topologi[M+1];
 	for(i=1; i<=M; i++)
@@ -112,7 +112,7 @@ int main() {
 		cin >> sken[i].x >> sken[i].y;
 	}
 
-	/* tes output */
+	/* tes output 
 	cout << "topologi" << endl;
 	for(i=1; i<=M; i++)
 	{
@@ -123,13 +123,14 @@ int main() {
 	{
 		cout << sken[i].x << " " << sken[i].y << endl;
 	}
+	*/
 
-	cout << "CHECKPOINT 2: INISIALISASI ROUTING TABLE"<<endl;
+	//cout << "CHECKPOINT 2: INISIALISASI ROUTING TABLE"<<endl;
 
 	A = new RoutingTable();
 	A->Initiate(top);
 	
-	/* tes output inisialisasi routing table*/
+	/* tes output inisialisasi routing table
 	for(i=1; i<=N; i++)
 	{
 		cout << "Node " << i << endl;
@@ -139,8 +140,9 @@ int main() {
 		}
 		cout << endl;
 	}
+	*/
 
-	cout << "CHECKPOINT 3: TRANSFER KNOWLEDGE"<<endl;
+	//cout << "CHECKPOINT 3: TRANSFER KNOWLEDGE"<<endl;
 
 
 
@@ -149,27 +151,57 @@ int main() {
 
 	/* ITERASI SEMUA SKENARIO */
 	for(i=1;i<=S;i++){
-		cout<< "kasus "<< i <<" :"<<endl;
-		cout << sken[i].x << " " << sken[i].y << endl;
+		//cout<< "skenario "<< i <<" :"<<endl;
+		//cout << sken[i].x << " " << sken[i].y << endl;
 
 		for(j = 1;j<=N;j++){
+			/* PROSES NODE UJI KE 1*/
+			//cout << "row tabel ke:"<< j << " :" <<endl;
 			//KASUS AD YG TIDAK TAHU
 			if(A->RT[sken[i].x].nodeInfo[j].distance == -1 && A->RT[sken[i].y].nodeInfo[j].distance != -1){
-				A->RT[sken[i].x].nodeInfo[j].distance += A->RT[sken[i].y].nodeInfo[j].distance;
+				//cout << "update pengetahuan node "<< sken[i].x << " row ke " << j <<endl;
+				A->RT[sken[i].x].nodeInfo[j].distance = A->RT[sken[i].y].nodeInfo[j].distance + 1;
 				A->RT[sken[i].x].nodeInfo[j].nextHop = sken[i].y;
+
+				//cout << j << " " << A->RT[sken[i].x].nodeInfo[j].distance << " " << A->RT[sken[i].x].nodeInfo[j].nextHop << endl;
 			}
 
 			//KASUS AD YG BEDA INFORMASINYA
 			if(A->RT[sken[i].x].nodeInfo[j].distance != -1 && A->RT[sken[i].y].nodeInfo[j].distance != -1 && A->RT[sken[i].x].nodeInfo[j].nextHop != A->RT[sken[i].y].nodeInfo[j].nextHop ){
 				if(A->RT[sken[i].x].nodeInfo[j].distance < A->RT[sken[i].y].nodeInfo[j].distance){
+					//cout << "pergantian pengetahuan node " << sken[i].x << " row ke " << j <<endl;
 					A->RT[sken[i].y].nodeInfo[j].distance = A->RT[sken[i].x].nodeInfo[j].distance + 1;
-					A->RT[sken[i].y].nodeInfo[j].nextHop = A->RT[sken[i].x].nodeInfo[j].nextHop;
+					A->RT[sken[i].y].nodeInfo[j].nextHop = sken[i].x;
+
+					//cout << j << " " << A->RT[sken[i].x].nodeInfo[j].distance << " " << A->RT[sken[i].x].nodeInfo[j].nextHop << endl;
+				}
+			}
+
+			/* PROSES NODE UJI KE 2*/
+			//KASUS AD YG TIDAK TAHU
+			if(A->RT[sken[i].y].nodeInfo[j].distance == -1 && A->RT[sken[i].x].nodeInfo[j].distance != -1){
+				//cout << "update pengetahuan node "<< sken[i].y << " row ke " << j <<endl;
+				A->RT[sken[i].y].nodeInfo[j].distance = A->RT[sken[i].x].nodeInfo[j].distance + 1;
+				A->RT[sken[i].y].nodeInfo[j].nextHop = sken[i].x;
+
+				//cout << j << " " << A->RT[sken[i].y].nodeInfo[j].distance << " " << A->RT[sken[i].x].nodeInfo[j].nextHop << endl;
+			}
+
+			//KASUS AD YG BEDA INFORMASINYA
+			if(A->RT[sken[i].y].nodeInfo[j].distance != -1 && A->RT[sken[i].x].nodeInfo[j].distance != -1 && A->RT[sken[i].y].nodeInfo[j].nextHop != A->RT[sken[i].x].nodeInfo[j].nextHop ){
+				if(A->RT[sken[i].y].nodeInfo[j].distance < A->RT[sken[i].x].nodeInfo[j].distance){
+					//cout << "pergantian pengetahuan node " << sken[i].y << " row ke " << j <<endl;
+					A->RT[sken[i].x].nodeInfo[j].distance = A->RT[sken[i].y].nodeInfo[j].distance + 1;
+					A->RT[sken[i].x].nodeInfo[j].nextHop = sken[i].y;
+
+					//cout << j << " " << A->RT[sken[i].y].nodeInfo[j].distance << " " << A->RT[sken[i].x].nodeInfo[j].nextHop << endl;
 				}
 			}
 		}
+		//cout<<endl;
 	}
 
-	/* tes output */
+	/* tes FINAL STATE 
 	for(i=1; i<=N; i++)
 	{
 		cout << "Node " << i << endl;
@@ -179,8 +211,16 @@ int main() {
 		}
 		cout << endl;
 	}
+	*/
+	/* OUTPUT GRADER */
+	for(i=1; i<=N; i++)
+	{
+		for(j=1; j<=N; j++)
+		{
+			cout <<A->RT[i].nodeInfo[j].distance << " " << A->RT[i].nodeInfo[j].nextHop << endl;
+		}
+	}
 
-	/*  */
 
 	return 0;
 }
